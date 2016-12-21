b04541b59ab72f995b50eb12431975d5c8f20e41
¨nesvietaiev¨ <nesvietaiev@gmail.com>
2016-12-21 19:17:05 +0200

    Notepad v1.0

--- link.rb
+++ link.rb
@@ -9,9 +9,17 @@ class Link < Post
   end
 
   def read_from_console
+    puts "Адрес ссылки:"
+    @url = STDIN.gets.chomp
+
+    puts "Описание ссылки:"
+    @text = STDIN.gets.chomp
   end
 
   def to_strings
+    time_string = "Создано: #{@created_at.strftime("%Y.%m.%d, %H:%M:%S")} \n\r \n\r"
+
+    return [@url, @text, time_string]
   end
 
 end
---
+++ notepad.rb
@@ -0,0 +1,28 @@
+require_relative 'post.rb'
+require_relative 'memo.rb'
+require_relative 'link.rb'
+require_relative 'task.rb'
+
+puts "Привет! Я твой блокнот!"
+puts "Что хотитет записать в блокнот?"
+
+choises = Post.post_types
+
+choise = -1
+
+until choise >= 0 && choise < choises.size
+
+  choises.each_with_index do |type, index|
+    puts "\t#{index}. #{type}"
+  end
+
+  choise = STDIN.gets.chomp.to_i
+end
+
+entry = Post.create(choise)
+
+entry.read_from_console
+
+entry.save
+
+puts "Ваша запись сохранена"
